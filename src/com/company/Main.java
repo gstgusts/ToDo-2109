6a93146dfea81a5db510dcde389bc322ca768766
Gusts Linkevics <gusts@gst.lv>
2020-10-19 19:40:36 +0300

    Refactored data getting

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,25 @@
 package com.company;
 
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Main {
 
     public static void main(String[] args) {
         ToDoItemRepository repo = new ToDoItemRepository();
-        var importantItems = repo.getAllItemsForDay();
+        var importantItems = repo.getDoneItemsForLast7Days();
 
         for (var item :
                 importantItems) {
             DateTimeFormatter df = DateTimeFormatter.ofPattern("dd-MM-yyyy");
             System.out.println(item.getName() +" "+ item.getDueDate().format(df));
         }
+
+
+
+        var todo1 = repo.getById(3);
+
+        System.out.println(todo1.getName());
+
     }
 }
--- src/com/company/ToDoItemRepository.java
+++ src/com/company/ToDoItemRepository.java
@@ -12,26 +12,19 @@ public class ToDoItemRepository {
     private static final String userName = "todo";
     private static final String password = "todo123";
 
+    private Connection connection;
+
     public List<ToDo> getAllImportantItems() {
         var sqlText = "SELECT * FROM todo.todo_items where ti_is_important = 1 and ti_is_done = 0";
 
-        List<ToDo> items = new ArrayList<>();
-
         try {
-            Connection connection = DriverManager.getConnection(connectionUrl, userName, password);
-            PreparedStatement ps = connection.prepareStatement(sqlText);
-
-            ResultSet rs = ps.executeQuery();
-
-            while(rs.next()) {
-                items.add(ToDo.create(rs));
-            }
-
+            var ps = getPreparedStatement(sqlText);
+            return getItems(ps);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
 
-        return items;
+        return new ArrayList<>();
     }
 
     public List<ToDo> getAllItemsForDay() {
@@ -44,25 +37,78 @@ public class ToDoItemRepository {
     public List<ToDo> getAllItemsForDay(LocalDateTime from, LocalDateTime to) {
         var sqlText = "select * from todo.todo_items where ti_is_done = 0 and (ti_due_date between ? and ?)";
 
-        List<ToDo> items = new ArrayList<>();
+        List<ToDo> ps = getItems(from, to, sqlText);
+        if (ps != null) return ps;
+
+        return new ArrayList<>();
+    }
+
+    public List<ToDo> getDoneItemsForLast7Days() {
+        return getDoneItemsForLast7Days(LocalDateTime.now());
+    }
+
+    public List<ToDo> getDoneItemsForLast7Days(LocalDateTime to) {
+
+        var from = to.minusDays(7);
+        var sqlText = "select * from todo.todo_items where ti_is_done = 1 and (ti_finished_date between ? and ?)";
+
+        List<ToDo> ps = getItems(from, to, sqlText);
+        if (ps != null) return ps;
+
+        return new ArrayList<>();
+    }
+
+    public ToDo getById(int id) {
+
+        var sqlText = "select * from todo.todo_items where ti_id = ?";
 
         try {
-            Connection connection = DriverManager.getConnection(connectionUrl, userName, password);
-            PreparedStatement ps = connection.prepareStatement(sqlText);
-
-            ps.setTimestamp(1, Timestamp.valueOf(from));
-            ps.setTimestamp(2, Timestamp.valueOf(to));
-
-            ResultSet rs = ps.executeQuery();
-
-            while(rs.next()) {
-                items.add(ToDo.create(rs));
-            }
+            var ps = getPreparedStatement(sqlText);
+            ps.setInt(1, id);
+            return getItem(ps);
 
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
 
+        return null;
+    }
+
+    private PreparedStatement getPreparedStatement(String sqlText) throws SQLException {
+        connection = DriverManager.getConnection(connectionUrl, userName, password);
+        return connection.prepareStatement(sqlText);
+    }
+
+    private ToDo getItem(PreparedStatement ps) throws SQLException {
+        var items = getItems(ps);
+        return items.get(0);
+    }
+
+    private List<ToDo> getItems(PreparedStatement ps) throws SQLException {
+
+        List<ToDo> items = new ArrayList<>();
+
+        ResultSet rs = ps.executeQuery();
+
+        while(rs.next()) {
+            items.add(ToDo.create(rs));
+        }
+
         return items;
+    }
+
+    private List<ToDo> getItems(LocalDateTime from, LocalDateTime to, String sqlText) {
+        try {
+            var ps = getPreparedStatement(sqlText);
+
+            ps.setTimestamp(1, Timestamp.valueOf(from));
+            ps.setTimestamp(2, Timestamp.valueOf(to));
+
+            return getItems(ps);
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return null;
     }
 }
